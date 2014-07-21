f12556e1b819f1c0ac34256e89dfcf0148ff5c15
Luke Karrys <lukekarrys@gmail.com>
2014-07-21 10:07:44 -0700

    use moment for timeAgo derived property on howls model

--- client/models/howl.js
+++ client/models/howl.js
@@ -1,8 +1,23 @@
 var Model = require('ampersand-model');
+var moment = require('moment');
 
 module.exports = Model.extend({
     props: {
         content: 'string',
         createdAt: 'date'
     },
+    derived: {
+        moment: {
+            deps: ['createdAt'],
+            fn: function () {
+                return moment(this.createdAt);
+            }
+        },
+        timeAgo: {
+            deps: ['moment'],
+            fn: function () {
+                return this.moment.fromNow();
+            }
+        }
+    }
 });
